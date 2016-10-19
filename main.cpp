60a622b9760ea124ec38a8673e2d1b8be1f3bde3
mon3 <seniut.monika@gmail.com>
2016-10-19 17:07:14 +0200

    Working, but so slow...

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+
+
+int main(){
+	double a = 0.38*pow(10.0,-9.0);
+	cout<< "a= "<< a<< endl;
+// char plik[]="wyjscie.txt";
+
+
+// int nr_line=2;
+// Osobowy o1;
+// o1.Wypisz();
+// Pojazd *o2=new Osobowy("Opel",10300.55,5,1500);
+// o2->Wypisz();
+// o1.Wczytaj("pojazdy.txt",nr_line);
+// o1.Wypisz();
+// o2->Zapisz(plik);
+// 	Osobowy macierz[2][3];
+
+// for (int i=0;i<2;i++){
+// for (int j=0;j<3;j++){
+// 	macierz[i][j]=Osobowy();
+
+// }
+// 	}
+// 	macierz[0][0].zmien_cena(8);
+// for (int i=0;i<2;i++){
+// for (int j=0;j<3;j++){
+// 	macierz[i][j].Wypisz();
+
+// }
+// 	}
+
+	return 0;
+}
